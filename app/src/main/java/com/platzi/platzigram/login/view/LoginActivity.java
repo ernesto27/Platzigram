d6748da57a462036292be84344fe956820d539fb
ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-09-24 22:21:04 -0300

    Se agregan carpetas mvp arquitectura

---
+++ app/src/main/java/com/platzi/platzigram/login/view/LoginActivity.java
@@ -0,0 +1,90 @@
+package com.platzi.platzigram.login.view;
+
+import android.content.Intent;
+import android.support.design.widget.TextInputEditText;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ProgressBar;
+import android.widget.Toast;
+
+import com.platzi.platzigram.R;
+import com.platzi.platzigram.login.presenter.LoginPresenter;
+import com.platzi.platzigram.login.presenter.LoginPresenterImpl;
+import com.platzi.platzigram.view.ContainerActivity;
+import com.platzi.platzigram.view.CreateAccountActivity;
+
+public class LoginActivity extends AppCompatActivity implements LoginView{
+
+    private TextInputEditText username;
+    private TextInputEditText password;
+    private Button login;
+    private ProgressBar progressBar;
+    private LoginPresenter loginPresenter;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_login);
+
+        username = (TextInputEditText) findViewById(R.id.username);
+        password = (TextInputEditText) findViewById(R.id.password);
+        login = (Button) findViewById(R.id.login);
+        progressBar = (ProgressBar) findViewById(R.id.progressbarLogin);
+        hideProgressBar();
+        loginPresenter = new LoginPresenterImpl(this);
+        login.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                loginPresenter.signIn(username.getText().toString(), password.getText().toString());
+
+            }
+        });
+    }
+
+
+    @Override
+    public void enableInputs() {
+        username.setEnabled(true);
+        password.setEnabled(true);
+        login.setEnabled(true);
+    }
+
+    @Override
+    public void disableInputs() {
+        username.setEnabled(false);
+        password.setEnabled(false);
+        login.setEnabled(false);
+    }
+
+    @Override
+    public void showProgressBar() {
+        progressBar.setVisibility(View.VISIBLE);
+    }
+
+    @Override
+    public void hideProgressBar() {
+        progressBar.setVisibility(View.GONE);
+    }
+
+    @Override
+    public void loginError(String error) {
+        Toast.makeText(this, "Ocurrio un error", Toast.LENGTH_LONG).show();
+    }
+
+    @Override
+    public void goCreateAccount() {
+        Intent intent = new Intent(this, CreateAccountActivity.class);
+        startActivity(intent);
+    }
+
+    @Override
+    public void goHome() {
+            Intent intent = new Intent(this, ContainerActivity.class);
+            startActivity(intent);
+    }
+
+
+
+}
--- app/src/main/java/com/platzi/platzigram/post/view/HomeFragment.java
+++ app/src/main/java/com/platzi/platzigram/post/view/HomeFragment.java
@@ -1,15 +1,22 @@
-package com.platzi.platzigram.view.fragment;
+package com.platzi.platzigram.post.view;
 
 
 
+import android.content.Intent;
 import android.graphics.Color;
 import android.graphics.drawable.ColorDrawable;
+import android.net.Uri;
 import android.os.Bundle;
+import android.os.Environment;
+import android.provider.MediaStore;
+import android.support.design.widget.FloatingActionButton;
 import android.support.v4.app.Fragment;
+import android.support.v4.content.FileProvider;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.Toolbar;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
@@ -18,14 +25,18 @@ import com.platzi.platzigram.R;
 import com.platzi.platzigram.adapter.PictureAdapterRecyclerView;
 import com.platzi.platzigram.model.Picture;
 
+import java.io.File;
+import java.io.IOException;
+import java.text.SimpleDateFormat;
 import java.util.ArrayList;
+import java.util.Date;
 
-/**
- * A simple {@link Fragment} subclass.
- *
- */
+
 public class HomeFragment extends Fragment {
 
+    private static final int REQUEST_CAMERA = 1;
+    private FloatingActionButton fabCamera;
+    private String photoPathTemp = "";
 
     public HomeFragment() {
         // Required empty public constructor
@@ -39,6 +50,7 @@ public class HomeFragment extends Fragment {
         View view = inflater.inflate(R.layout.fragment_home, container, false);
         showToolbar(getResources().getString(R.string.tab_home), false, view);
         RecyclerView picturesRecycler = (RecyclerView) view.findViewById(R.id.pictureRecycler);
+        fabCamera = (FloatingActionButton) view.findViewById(R.id.fabcamera);
 
         LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
         linearLayoutManager.setOrientation(LinearLayoutManager.VERTICAL);
@@ -50,7 +62,58 @@ public class HomeFragment extends Fragment {
         picturesRecycler.setAdapter(pictureAdapterRecyclerView);
 
 
+        fabCamera.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                takePicture();
+            }
+        });
+
         return view;
+    }
+
+    private void takePicture() {
+        Intent intentTakePicture = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        if(intentTakePicture.resolveActivity(getActivity().getPackageManager()) != null){
+
+            File photoFile = null;
+
+            try {
+                photoFile = createImageFile();
+            }catch (Exception e){
+                e.printStackTrace();
+            }
+
+            if(photoFile != null){
+                Uri photoUri = FileProvider.getUriForFile(getActivity(), "com.platzi.platzigram", photoFile);
+                intentTakePicture.putExtra(MediaStore.EXTRA_OUTPUT, photoUri);
+                startActivityForResult(intentTakePicture, REQUEST_CAMERA);
+            }
+
+        }
+    }
+
+    private File createImageFile() throws IOException {
+        String  timeStamp = new SimpleDateFormat("yyyyMMdd_HH-mm-ss").format(new Date());
+        String imageFileName = "JPEG_" + timeStamp + "_";
+        File storageDirectory = getActivity().getExternalFilesDir(Environment.DIRECTORY_PICTURES);
+
+        File photo = File.createTempFile(imageFileName, ".jpg", storageDirectory);
+        photoPathTemp = "file:" + photo.getAbsolutePath();
+
+
+        return photo;
+    }
+
+    @Override
+    public void onActivityResult(int requestCode, int resultCode, Intent data) {
+        //super.onActivityResult(requestCode, resultCode, data);
+        if(requestCode == REQUEST_CAMERA && resultCode == getActivity().RESULT_OK ){
+            Log.d("Home fragment", "Camera ok!");
+            Intent i = new Intent(getActivity(), NewPostActivity.class);
+            i.putExtra("IMAGE_PATH_TEMP", photoPathTemp);
+            startActivity(i);
+        }
     }
 
     public ArrayList<Picture> buidPictures(){
