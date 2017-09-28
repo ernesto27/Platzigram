4cc76cae111d376464e5532082263b106b46650c
Ernesto ponce <ernesto@pigmalionstudios.com.ar>
2017-09-28 11:18:39 -0300

    Add Facebook login ,  gitignore

--- app/src/main/java/com/platzi/platzigram/login/view/LoginActivity.java
+++ app/src/main/java/com/platzi/platzigram/login/view/LoginActivity.java
@@ -11,6 +11,19 @@ import android.widget.Button;
 import android.widget.ProgressBar;
 import android.widget.Toast;
 
+import com.facebook.AccessToken;
+import com.facebook.CallbackManager;
+import com.facebook.FacebookCallback;
+import com.facebook.FacebookException;
+import com.facebook.FacebookSdk;
+
+import com.facebook.login.LoginResult;
+import com.facebook.login.widget.LoginButton;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthCredential;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FacebookAuthProvider;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
 import com.platzi.platzigram.R;
@@ -18,22 +31,29 @@ import com.platzi.platzigram.login.presenter.LoginPresenter;
 import com.platzi.platzigram.login.presenter.LoginPresenterImpl;
 import com.platzi.platzigram.view.ContainerActivity;
 
+import java.util.Arrays;
+
 public class LoginActivity extends AppCompatActivity implements LoginView{
 
     private TextInputEditText username;
     private TextInputEditText password;
     private Button login;
+    private LoginButton loginButtonFacebook;
     private ProgressBar progressBar;
     private LoginPresenter loginPresenter;
 
     private static final String TAG = "LoginActivity";
     private FirebaseAuth firebaseAuth;
     private  FirebaseAuth.AuthStateListener authStateListener;
+    private CallbackManager callbackManager;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        FacebookSdk.sdkInitialize(getApplicationContext());
+        callbackManager = CallbackManager.Factory.create();
 
 
         firebaseAuth = FirebaseAuth.getInstance();
@@ -44,6 +64,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
                 FirebaseUser firebaseUser = firebaseAuth.getCurrentUser();
                 if(firebaseUser != null){
                     Log.w(TAG, "Usuario logueado " + firebaseUser.getEmail());
+                    goHome();
                 }else{
                     Log.w(TAG, "Usuario no logueado");
                 }
@@ -53,6 +74,7 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
         username = (TextInputEditText) findViewById(R.id.username);
         password = (TextInputEditText) findViewById(R.id.password);
         login = (Button) findViewById(R.id.login);
+        loginButtonFacebook = (LoginButton) findViewById(R.id.login_facebook);
         progressBar = (ProgressBar) findViewById(R.id.progressbarLogin);
         hideProgressBar();
         loginPresenter = new LoginPresenterImpl(this);
@@ -61,6 +83,43 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
             public void onClick(View v) {
                 signIn(username.getText().toString(), password.getText().toString());
 
+            }
+        });
+
+
+        loginButtonFacebook.setReadPermissions(Arrays.asList("email"));
+        loginButtonFacebook.registerCallback(callbackManager, new FacebookCallback<LoginResult>() {
+            @Override
+            public void onSuccess(LoginResult loginResult) {
+                Log.w(TAG, "Facebook loging success Token: " + loginResult.getAccessToken().getToken());
+                signInFacebookFirebase(loginResult.getAccessToken());
+            }
+
+            @Override
+            public void onCancel() {
+                Log.w(TAG, "Cancel facebook login" );
+            }
+
+            @Override
+            public void onError(FacebookException error) {
+                Log.w(TAG, "Facebook login error " + error.toString());
+            }
+        });
+
+    }
+
+    private void signInFacebookFirebase(AccessToken token) {
+        AuthCredential authCredential = FacebookAuthProvider.getCredential(token.getToken());
+        firebaseAuth.signInWithCredential(authCredential).addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+            @Override
+            public void onComplete(@NonNull Task<AuthResult> task) {
+                if(task.isSuccessful()){
+                    goHome();
+                    Toast.makeText(LoginActivity.this, "Login facebook success", Toast.LENGTH_SHORT).show();
+                }else{
+                    Toast.makeText(LoginActivity.this, "Login facebook not success", Toast.LENGTH_SHORT).show();
+
+                }
             }
         });
     }
@@ -128,5 +187,10 @@ public class LoginActivity extends AppCompatActivity implements LoginView{
     }
 
 
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        callbackManager.onActivityResult(requestCode, resultCode, data);
+    }
 
 }
